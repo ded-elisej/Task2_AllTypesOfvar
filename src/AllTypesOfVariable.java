77f0aca12038572bbd9b901079bab5a7a4500f4a
Tatsiana Yeliseyeva <roimze@mail.ru>
2021-11-04 12:48:34 +0300

    task with all types of variables

---
+++ src/AllTypesOfVariable.java
@@ -0,0 +1,18 @@
+public class AllTypesOfVariable {
+    //In this task it is required to declare all types of variables and display it
+    public static void main(String[] args) {
+        int integerVariable = 100295;
+        double doubleVariable = 10.56;
+        byte leastVariable = -5;
+        short shortVariable = 300;
+        float floatVariable = 2362.4f;
+        long longestVariable = 123456789;
+        char simbolVariable1 = 36; //unicode
+        char simbolVariable2 = '$';
+        //String like a massive of characters
+        String string = "Hello, World";
+        System.out.println("int: " + integerVariable + "; " + "double: " + doubleVariable + "; " + "byte: " + leastVariable + "; ");
+        System.out.println("short: " + shortVariable + "; " + "float: " + floatVariable + "; " + "long: " + longestVariable + "; ");
+        System.out.println("char: " + simbolVariable1 + ", " + simbolVariable2 + "; " + "String: " + string + "; ");
+    }
+}
